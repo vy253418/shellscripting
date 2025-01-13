bc4d2396925cbf15a27013249df4403c10c1e0ba
veera <vy253418@gmail.com>
2025-01-13 23:00:51 +0530

    data types

--- 05_array.sh
+++ 05_array.sh
@@ -3,6 +3,8 @@
 Movies=("subbu" "venki" "kumar")
 #index starts from 0, size is 3 
 
-echo "the first movie is :$Movies[0]"
-echo "the second movie is:$Movies[1]"
-echo "the Third movie is: $Movies[2]"
+echo "the first movie is :${Movies[0]}"
+echo "the second movie is:${Movies[1]}"
+echo "the Third movie is: ${Movies[2]}"
+
+echo "we need list all movies=${Movies[@]}"
