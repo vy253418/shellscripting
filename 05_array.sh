111b037758afa635b9bdfe1602dc41404d76c6a4
veera <vy253418@gmail.com>
2025-01-13 22:58:08 +0530

    data types

---
+++ 05_array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Movies=("subbu" "venki" "kumar")
+#index starts from 0, size is 3 
+
+echo "the first movie is :$Movies[0]"
+echo "the second movie is:$Movies[1]"
+echo "the Third movie is: $Movies[2]"
