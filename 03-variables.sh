d245313aaa56f92e69fd03df6fd6542f7bf120a3
veera <vy253418@gmail.com>
2025-08-30 17:51:33 +0530

    variables added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Veera
+Person2=Suresh
+echo "$Person1: HI $Person2"
+echo "$Person2 : Hey Hi"
+echo "$Person1 : Where are you going"
+echo "$Person2 : i am going to Market"
+echo "$Person1 : i will also come wait"
