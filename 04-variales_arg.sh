e5b0157130cd7ffa0234e76c95baa6f09a1901ae
veera <vy253418@gmail.com>
2025-09-17 10:43:02 +0530

    arguments_added

---
+++ 04-variales_arg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: HI $Person2"
+echo "$Person2 : Hey Hi"
+echo "$Person1 : Where are you going"
+echo "$Person2 : i am going to Market"
+echo "$Person1 : i will also come wait"
