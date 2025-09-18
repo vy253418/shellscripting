e8a065f4453f8d8f65d4473b47753d7d02f5447c
veera <vy253418@gmail.com>
2025-09-18 11:14:22 +0530

    read variable

--- 06-Datatypes.sh
+++ 06-Datatypes.sh
@@ -6,3 +6,9 @@ NumberB=$2
 sum=$((NumberA+NumberB))
 
 echo "the total value is $sum"
+
+Timestamp=$(date)
+
+echo "the script executed time is $Timestamp
+
+echo "the value $NumberA and $NumberB is equal to $sum
