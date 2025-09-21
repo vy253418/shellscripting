1a49e65456ea67cb134deb92119cacb34624cbfc
veera <vy253418@gmail.com>
2025-09-21 22:06:48 +0530

    read variable

--- 09_conditions.sh
+++ 09_conditions.sh
@@ -4,7 +4,7 @@ Number=$1
 
 if [ $Number gt 100]
 then 
- echo "given number is greater then  100"
- else 
- echo "given number is less or equal  100"
+    echo "given number is greater then  100"
+else 
+    echo "given number is less or equal  100"
 fi
