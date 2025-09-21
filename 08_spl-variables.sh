8b2a7e4244a5929739444f1f75ec6badcf227bc7
veera <vy253418@gmail.com>
2025-09-21 21:09:40 +0530

    read variable

---
+++ 08_spl-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables passwd :$@"
+echo "Number of variables: $#"
+echo "script name :$0"
+echo " present working directory : $PWD"
+echo "home directory of current user : $HOME"
+echo "which user id running this script : $USER"
+echo "process id of current script : $$"
+sleep 60 &
+echo "process id of last command in background : $!
