7d843c1b653fe490a1baf2e632373ebbd660617f
veera <vy253418@gmail.com>
2025-09-21 16:26:52 +0530

    read variable

--- 07-array.sh
+++ 07-array.sh
@@ -4,7 +4,7 @@ MOVIES=("pushpa" "rrr" "Devara")
 #index starts from 0,size is 3
 
 echo "First movie :  ${MOVIES[0]}"
-echo "Second movie : $MOVIES[1]"
-echo "Third movie : $MOVIES[2]"
-echo "All movies :$MOVIES[@]"
+echo "Second movie : ${MOVIES[1]}"
+echo "Third movie : ${MOVIES[2]}"
+echo "All movies :${MOVIES[@]}"
 
