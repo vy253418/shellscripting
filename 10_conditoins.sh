7a89242269479129dc0edc72ccfb7102659dedff
veera <vy253418@gmail.com>
2025-09-23 11:17:06 +0530

    read variable

--- 10_conditoins.sh
+++ 10_conditoins.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+USERID=$(id -u)
 
-yum install mysql -y
+if [ $USERID -ne 0]
+then 
+   echo "ERROR::you have must sudo access to execute this script"
+else
+
+dnf install mysql -y
+
+fi
+
+
+
