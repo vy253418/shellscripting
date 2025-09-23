7e7418e10a35e758326500d42962b33e52ba1512
veera <vy253418@gmail.com>
2025-09-23 11:20:57 +0530

    read variable

--- 10_conditoins.sh
+++ 10_conditoins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 USERID=$(id -u)
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
    echo "ERROR::you have must sudo access to execute this script"
 fi
 
-dnf install mysqll -y
+dnf install mysqll -y 
 
 
 
