79ee2cc7161d0250be62f6ed696dd326a79d5ece
veera <vy253418@gmail.com>
2025-09-27 18:13:48 +0530

    read variable

--- 10_conditoins.sh
+++ 10_conditoins.sh
@@ -12,12 +12,19 @@ dnf install mysqll -y
 if [ $? -ne 0 ]
 then
   echo "ERROR: the previous command is Failuer"
-  
+ exit 1 
 else  
   echo "the previous command is success"
 fi  
 
 dnf install git -y
+
+if [ $? -ne 0 ]
+then  
+   echo "ERROR the previous command is Failuer"
+else 
+   echo "The previous command is Success"
+fi   
 
 
 
